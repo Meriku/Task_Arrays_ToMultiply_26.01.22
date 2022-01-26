64eae608d8dabf39e3ce757d36ebe35704b921a0
Professional <Professional@USER-PC>
2022-01-26 22:44:21 +0200

    Добавьте файлы проекта.

---
+++ Task_Arrays_ToMultiply_26.01.22/ArrayHandler.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2_2Dim_Array
+{
+    public class ArrayHandler
+    {
+
+        public static int[,] GetNewArray(int rowLenght, int columnLenght)  // Метод создающий новый массив по заданным параметрам.
+        {
+
+
+            int[,] newArray = new int[rowLenght, columnLenght]; // Создаем новый массив с заданным количеством рядов и столбцов.
+            var rnd = new Random();
+
+            var i = 0;
+            while (i < rowLenght)
+            {
+                var j = 0;
+                while (j < columnLenght)
+                {
+                    newArray[i, j] = rnd.Next(0, 36);
+                    j++;
+                }
+                i++;
+            }
+
+            return newArray;
+        }
+
+
+        public static List<int> GetOnlyDividingBySix(int[,] array) // Возвращает список чисел которые без остатка делятся на 6.                                                               
+        {
+            int rowLenght = array.GetLength(0);
+            int columnLenght = array.GetLength(1);
+
+            List<int> newArray = new List<int>(); // Создаем список в который будем записывать подходящие значения.
+
+            var i = 0;
+            while (i < columnLenght)
+            {
+
+                var j = 0;
+                while (j < rowLenght)
+                {
+                    if (array[j, i] > 0 && (array[j, i] % 6) == 0) // Если число больше 0 и делится без остатка на 6 - добавляем.
+                    {
+                        newArray.Add(array[j, i]);
+
+                    }
+                    j++;
+                }
+
+                i++;
+            }
+            return newArray;
+        }
+
+
+        public static long ToMultiplyAll(List<int> array) // Перемножаем все значения в списке. 
+        {
+            long result = 1;
+            var i = 0;
+
+            while (i < array.Count)
+            {
+                result *= array[i];
+                i++;
+            }
+
+            return result;
+        }
+
+
+
+        public static void ToPrintArray(int[,] array)  // Перегрузка - Выводим переданный двухмерный массив на консоль.
+        {
+            int rowLenght = array.GetLength(0);
+            int columnLenght = array.GetLength(1);
+
+            Console.Write("\n\tВаш двухмерный массив M:\n");
+
+            var i = 0;
+            while (i < columnLenght)
+            {
+                var j = 0;
+                while (j < rowLenght)
+                {
+                    Console.Write("\t" + array[j, i]);
+                    j++;
+                }
+                Console.Write("\n");
+                i++;
+            }
+
+        }
+
+        public static void ToPrintArray(int[] array)  // Перегрузка - Выводим переданный одномерный массив на консоль.
+        {
+            Console.Write("\n\tВаш одномерный массив L:\n");
+
+            var i = 0;
+            while (i < array.Length)
+            {
+                Console.Write("\t" + array[i]);
+                i++;
+            }
+
+        }
+
+
+        public static void ToPrintArray(List<int> array)  // Перегрузка - Выводим переданный список на консоль.
+        {
+            Console.Write("\n\tВаш одномерный массив L:\n");
+
+            var i = 0;
+            while (i < array.Count)
+            {
+                Console.Write("\t" + array[i]);
+                i++;
+            }
+
+        }
+
+    }
+
+
+
+}
---
+++ Task_Arrays_ToMultiply_26.01.22/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2_2Dim_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Задана целочисленная матрица M[m][n].
+            // Элементы этой матрицы, без остатка делящиеся на 6, переписать в массив L[] и найти их произведение. 
+            // Напечатать матрицу M, массив L и найденное произведение. Числа m и n вводятся с клавиатуры.
+
+
+            Console.WriteLine("Введите длину ряда матрицы");             // Получаем число М (длина ряда / количество столбцов).
+            var m = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите длину столбца матрицы");          // Получаем число N (длина столбца / количество рядов).
+            var n = int.Parse(Console.ReadLine());
+
+
+            var arrayM = ArrayHandler.GetNewArray(m, n);                 // Создаем двухмерный массив М заданных нами размеров.   
+            ArrayHandler.ToPrintArray(arrayM);                           // Выводим его
+
+            var arrayL = ArrayHandler.GetOnlyDividingBySix(arrayM);      // Создаем список, из значений массива М, которые делятся на 6 без остатка.  
+            ArrayHandler.ToPrintArray(arrayL);                           // Выводим его
+
+            var MultiplyResult = ArrayHandler.ToMultiplyAll(arrayL);     // Перемножаем все значения списка. 
+            Console.WriteLine($"\n\n\tРезультат умножения всех значений массива L: {MultiplyResult}");
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
